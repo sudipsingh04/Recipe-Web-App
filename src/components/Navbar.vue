b7789332a86e8b24d941b63a348978a739c5c84e
sudipsingh04 <sudipsingh04@gmail.com>
2020-05-07 18:08:38 +0530

    Added navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="navbar">
+        <nav class="nav-extended indigo darken-2">
+            <div class="nav-content">
+                <router-link to="">
+                    <span class="nav-title">Vue Crud</span>
+                </router-link>
+                <a href="" class="btn-floating btn-large halfway-fab pink">
+                    <router-link to="">
+                        <i class="material-icons">add</i>
+                    </router-link>
+                </a>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Nabvar',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+.navbar nav{
+  padding: 0 20px;
+}
+</style>
