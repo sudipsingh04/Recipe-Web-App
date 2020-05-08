11d24e274b85cc38ae624e2898a2352b438a588e
sudipsingh04 <sudipsingh04@gmail.com>
2020-05-08 13:54:14 +0530

    Fetch data to edit

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -1,16 +1,27 @@
 <template>
-  <div class="edit-smoothie container">
-    <h2>Edit a smoothie {{ this.$route.params.smoothie_slug }}</h2>
+  <div v-if="smoothie" class="edit-smoothie container">
+    <h2>Edit {{ smoothie.title }}</h2>
   </div>
 </template>
 
 <script>
+import db from '@/firebase/init'
+
 export default {
   name: 'EditSmoothie',
   data(){
     return{
-
+      smoothie: null
     }
+  },
+  created(){
+    let ref = db.collection('smoothies').where('slug', '==', this.$route.params.smoothie_slug)
+    ref.get().then(sanpshot => {
+      sanpshot.forEach(doc => {
+        this.smoothie = doc.data()
+        this.smoothie.id = doc.id
+      });
+    })
   }
 }
 </script>
